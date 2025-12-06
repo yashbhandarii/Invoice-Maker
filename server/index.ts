5cf6c832f9e6d4bee3968957da441853d963d078
Yash Bhandari <yashbhandari0810@gmail.com>
2025-12-06 15:25:29 +0530

    Fix Vercel static file serving and rewrites

--- server/index.ts
+++ server/index.ts
@@ -75,9 +75,9 @@ export async function setupApp() {
       throw err;
     });
 
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === "production" && !process.env.VERCEL) {
       serveStatic(app);
-    } else {
+    } else if (process.env.NODE_ENV !== "production") {
       const { setupVite } = await import("./vite");
       await setupVite(httpServer, app);
     }
